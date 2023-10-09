3cc5ffa143f650e4a55bd08b0d791c10275cb6a2
SANZHAR ASSET <96543605+111sanz111@users.noreply.github.com>
2023-10-09 09:37:41 +0600

    Add files via upload

---
+++ apes.js
@@ -0,0 +1,20 @@
+
+const ape1 = {
+  name: 'Sanzhar',
+  age: 18
+};
+
+const ape2 = {
+  name: 'Ruslan',
+  age: 18
+};
+
+const ape3 = {
+  name: 'Nikita',
+  age: 19
+};
+
+
+const apes = [ape1, ape2, ape3]  
+  for (let ape of apes)
+    console.log(ape, ape.name)
